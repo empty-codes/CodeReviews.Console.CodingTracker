d07a58fafb967c62dd4fbe993b00cd71397fe90f
empty-codes <92234322+empty-codes@users.noreply.github.com>
2024-09-11 14:07:50 +0100

    Created config file and CodingSession Class

---
+++ CodingTracker.empty-codes/Models/CodingSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.empty_codes.Models
+{
+    internal class CodingSession
+    {
+        public int? Id { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public TimeSpan? Duration { get; set; }
+    }
+}
---
+++ CodingTracker.empty-codes/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using Microsoft.Data.Sqlite;
+
+string connectionString = ConfigurationManager.ConnectionStrings["CodingSessionDb"].ConnectionString;
+
+string dbPath = ConfigurationManager.AppSettings["DatabasePath"];
+string dateFormat = ConfigurationManager.AppSettings["DateFormat"];
+
