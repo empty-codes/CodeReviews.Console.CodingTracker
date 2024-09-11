322d0c9ef4c0fd13285dfbee660d43668c1e73ac
empty-codes <92234322+empty-codes@users.noreply.github.com>
2024-09-11 17:16:03 +0100

    Created Controller class and according CRUD methods

---
+++ CodingTracker.empty-codes/Controllers/CodingController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Dapper;
+using CodingTracker.empty_codes.Models;
+
+namespace CodingTracker.empty_codes.Controllers
+{
+    internal class CodingController
+    {
+        public string ConnectionString;
+
+        public CodingController(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public void InsertSession(CodingSession session)
+        {
+            using var conn = new SqliteConnection(ConnectionString);
+            string insertQuery = "INSERT INTO CodingSessions(StartTime, EndTime, Duration) VALUES(@start, @end, @duration)";
+            try
+            {
+                conn.Execute(insertQuery, session);
+
+                string getIdQuery = "SELECT last_insert_rowid();";
+                session.Id = conn.ExecuteScalar<int>(getIdQuery);
+
+                Console.WriteLine($"Session successfully added. (Session Id: {session.Id})\n");
+            }
+            catch (SqliteException e)
+            {
+                Console.WriteLine($"Error occured while trying to insert your session\n - Details: {e.Message}");
+            }
+        }
+
+        public List<CodingSession> ViewAllSessions()
+        {
+            var sessions = new List<CodingSession>;
+            using var conn = new SqliteConnection(ConnectionString);
+            string readQuery = "SELECT * FROM CodingSessions";
+            try
+            {
+                sessions = conn.Query<CodingSession>(readQuery).ToList();
+            }
+            catch (SqliteException e)
+            {
+                Console.WriteLine("Error occured while trying to access your sessions\n - Details: " + e.Message);
+            }
+            return sessions;
+        }
+
+        public void UpdateSession(CodingSession session)
+        {
+            using var conn = new SqliteConnection(ConnectionString);
+            string updateQuery = "UPDATE CodingSessions SET StartTime = @start, EndTime = @end, Duration = @duration WHERE Id = @id";
+            try
+            {
+                int result = conn.Execute(updateQuery, session);
+
+                if(result == 0)
+                {
+                    Console.WriteLine($"No session found with the provided Id: {session.Id}\n");
+                }
+                else
+                {
+                    Console.WriteLine($"Session with Id: {session.Id} successfully updated.\n");
+                }
+            }
+            catch (SqliteException e)
+            {
+                Console.WriteLine($"Error occured while trying to update your session\n - Details: {e.Message}");
+            }
+        }
+
+        public void DeleteSession(CodingSession session)
+        {
+            using var conn = new SqliteConnection(ConnectionString);
+            string deleteQuery = "DELETE FROM CodingSessions WHERE Id = @id";
+            try
+            {
+                int result = conn.Execute(deleteQuery, session);
+
+                if (result == 0)
+                {
+                    Console.WriteLine($"No session found with the provided Id: {session.Id}\n");
+                }
+                else
+                {
+                    Console.WriteLine($"Session with Id: {session.Id} successfully deleted.\n");
+                }
+            }
+            catch (SqliteException e)
+            {
+                Console.WriteLine($"Error occured while trying to delete your session\n - Details: {e.Message}");
+            }
+        }
+    }
+}
--- CodingTracker.empty-codes/Program.cs
+++ CodingTracker.empty-codes/Program.cs
@@ -1,9 +1,36 @@
 ﻿using System.Configuration;
 using System.Collections.Specialized;
 using Microsoft.Data.Sqlite;
+using Dapper; 
 
-string connectionString = ConfigurationManager.ConnectionStrings["CodingSessionDb"].ConnectionString;
+string? connectionString = ConfigurationManager.ConnectionStrings["CodingSessionDb"].ConnectionString;
 
-string dbPath = ConfigurationManager.AppSettings["DatabasePath"];
-string dateFormat = ConfigurationManager.AppSettings["DateFormat"];
+string? dbPath = ConfigurationManager.AppSettings["DatabasePath"];
+string? dateFormat = ConfigurationManager.AppSettings["DateFormat"];
+
+CreateDatabase(connectionString);
+
+void CreateDatabase(string connectionString)
+{
+    using var conn = new SqliteConnection(connectionString);
+
+    var createTableQuery = @"
+            CREATE TABLE IF NOT EXISTS CodingSessions (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                StartTime TEXT NOT NULL,
+                EndTime TEXT NOT NULL,
+                Duration TEXT NOT NULL
+            );";
+    try
+    {
+        conn.Execute(createTableQuery);
+    }
+    catch (SqliteException e)
+    {
+        Console.WriteLine($"Error occured while trying to create the database Table\n - Details: {e.Message}");
+    }
+    Console.WriteLine($"Database file {dbPath} successfully created.\n");
+}
+
+
 
